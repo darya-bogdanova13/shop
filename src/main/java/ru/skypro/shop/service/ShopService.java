9a73bd617baf0d7e40e1f8fbd44684ccb7a6b2de
darya <darya.kolycheva98@yandex.ru>
2024-06-27 14:22:05 +0300

    ДЗ готово

---
+++ src/main/java/ru/skypro/shop/service/ShopService.java
@@ -0,0 +1,8 @@
+package ru.skypro.shop.service;
+
+import java.util.List;
+
+public interface ShopService {
+    List<Integer> add(List<Integer> goods);
+    List<Integer> get(List<Integer> goods);
+}
---
+++ src/main/java/ru/skypro/shop/service/ShoppingCart.java
@@ -0,0 +1,8 @@
+package ru.skypro.shop.service;
+
+import java.util.List;
+
+public interface ShoppingCart {
+    List<Integer> add(List<Integer> goods);
+    List<Integer> get(List<Integer> goods);
+}
